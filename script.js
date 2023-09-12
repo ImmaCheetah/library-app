1e63a4c01fc5198e1908afecc6c888335dce992d
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-09-12 15:55:18 -0400

    Create new book with user input and push to array

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-const myLibrary = ['GOT', 'X detective', '4th monke'];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = "not read yet"
+    this.read = Boolean(read)
     this.info = function() {
         return `${title} by ${author}, ${pages} pages, ${read}`;
     }
 }
 
 function addBookToLibrary() {
-    let x = prompt("this is a test lord help me");
+    const book1 = new Book(prompt('title?'), prompt('author?'), prompt('pages?'), prompt('read?'));
 
-    console.log(x);
+    console.log(book1);
+    console.log(book1.info());
+    myLibrary.push(book1);
+    console.log(myLibrary[0]);
 }
 
 addBookToLibrary();
