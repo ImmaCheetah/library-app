4f476627e45f54feadb4eadcb97fd5df8248ad30
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-09-13 11:02:59 -0400

    Move info function to be in prototype of Book

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ function Book(title, author, pages, readState) {
     this.author = author
     this.pages = pages
     this.readState = readState
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${readState}`; //
-    }
+    // this.info = function() {
+    //     return `${title} by ${author}, ${pages} pages, ${readState}`;
+    // }
+}
+
+// Set info function to prototype of Book
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readState}`;
 }
 
 // Creates new variable each time it's called and adds new object to myLibrary array
 function addBookToLibrary() {
     const book = new Book(prompt('title?'), prompt('author?'), prompt('pages?'), prompt('read?'));
     myLibrary.push(book);
+    console.log(book.info());
 }
 
-addBookToLibrary();
 addBookToLibrary();
 
 
