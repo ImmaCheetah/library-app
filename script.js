81ec879b9b946ce2292bd5f385630268dff5a948
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-09-13 10:40:54 -0400

    Change read state into variable and use console log to test add function

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = Boolean(read)
+    this.read = read
     this.info = function() {
         return `${title} by ${author}, ${pages} pages, ${read}`;
     }
 }
 
 function addBookToLibrary() {
-    const book1 = new Book(prompt('title?'), prompt('author?'), prompt('pages?'), prompt('read?'));
+    const book = new Book(prompt('title?'), prompt('author?'), prompt('pages?'), prompt('read?'));
 
-    console.log(book1);
-    console.log(book1.info());
-    myLibrary.push(book1);
-    console.log(myLibrary[0]);
+    console.log(book);
+    console.log(book.info());
+    myLibrary.push(book);
+    console.log(myLibrary[0], myLibrary[1]);
 }
 
 addBookToLibrary();
+addBookToLibrary();
+
+// const arr = [];
+// function foo() {
+//   const a = Math.floor(Math.random() * 10);
+//   arr.push(a);
+// }
+
